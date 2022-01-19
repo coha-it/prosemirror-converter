de4a781b761028f1eb11001aff76f246080228fc
abachschmid <>
2022-01-19 03:01:29 +0100

    Custom Image added

--- app/Http/Controllers/ConvertCtrl.php
+++ app/Http/Controllers/ConvertCtrl.php
@@ -2,6 +2,34 @@
 
 namespace App\Http\Controllers;
 
+class CustomImage extends \HtmlToProseMirror\Nodes\Image
+{
+    public function data()
+    {
+        return [
+            'type' => 'image',
+            'attrs' => [
+
+                /*
+                drive-id": "d5856ed9-1355-48e7-b254-df5d82a255c6",
+                height": 400,
+                width": 1200,
+                file-type": "image/jpeg"
+                */
+                'height' => $this->DOMNode->hasAttribute('height') ? $this->DOMNode->getAttribute('height') : "400",
+                'width' => $this->DOMNode->hasAttribute('width') ? $this->DOMNode->getAttribute('width') : "1200",
+                'file-type' => $this->DOMNode->hasAttribute('file-type') ? $this->DOMNode->getAttribute('file-type') : "image/jpeg",
+                'drive-id' => $this->DOMNode->hasAttribute('drive-id') ? $this->DOMNode->getAttribute('drive-id') : "",
+                // 'drive-id' => $this->DOMNode->hasAttribute('src') ? $this->DOMNode->getAttribute('src') : "",
+
+                'alt' => $this->DOMNode->hasAttribute('alt') ? $this->DOMNode->getAttribute('alt') : null,
+                'src' => $this->DOMNode->hasAttribute('src') ? $this->DOMNode->getAttribute('src') : null,
+                'title' => $this->DOMNode->hasAttribute('title') ? $this->DOMNode->getAttribute('title') : null,
+            ],
+        ];
+    }
+}
+
 class CustomBold extends \HtmlToProseMirror\Marks\Bold
 {
     public function data()
@@ -59,7 +87,8 @@ class ConvertCtrl extends Controller
             \HtmlToProseMirror\Nodes\CodeBlock::class,
             \HtmlToProseMirror\Nodes\HardBreak::class,
             \HtmlToProseMirror\Nodes\Heading::class,
-            \HtmlToProseMirror\Nodes\Image::class,
+            // \HtmlToProseMirror\Nodes\Image::class,
+            CustomImage::class,
             \HtmlToProseMirror\Nodes\ListItem::class,
             \HtmlToProseMirror\Nodes\OrderedList::class,
             \HtmlToProseMirror\Nodes\Paragraph::class,
